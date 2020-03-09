d3e8ec7b318fd4314303a0d20041c8df99e9e2ac
Deeprand <qubicand@gmail.com>
2020-03-09 16:45:52 +0800

    delete: stop track some trivial files

--- draft.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main() {
-    char c1 = 'a', c2 = 'b', c3 = 'c', c4 = '\101', c5 = 101;
-    printf("a%c b%c \tc%c\tabc\n", c1, c2, c3);
-    printf("%c %c\n", c4, c5);
-    return 0;
-}
